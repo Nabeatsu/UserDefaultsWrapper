d99989aa3def2086646d4acde24cee4f6aa8b225
nabeatsu <tanabe.1478@gmail.com>
2020-02-05 23:08:57 +0900

    initial commit

---
+++ UserDefaultsWrapper/UserDefaultsWrapper.h
@@ -0,0 +1,19 @@
+//
+//  UserDefaultsWrapper.h
+//  UserDefaultsWrapper
+//
+//  Created by tanabe.nobuyuki on 2020/02/05.
+//  Copyright © 2020 tanabe.nobuyuki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserDefaultsWrapper.
+FOUNDATION_EXPORT double UserDefaultsWrapperVersionNumber;
+
+//! Project version string for UserDefaultsWrapper.
+FOUNDATION_EXPORT const unsigned char UserDefaultsWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserDefaultsWrapper/PublicHeader.h>
+
+
